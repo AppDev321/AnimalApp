1eb151959caaac2d02b9519e1b8e49c18c53a637
NomanHome <noman@test.com>
2023-07-04 23:37:40 +0500

    updated 25-6-2023

--- app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
@@ -54,9 +54,9 @@ class LoginActivity : AppCompatActivity() {
 
             val email = findViewById<EditText>(R.id.editTextEmail)
             val password = findViewById<EditText>(R.id.editTextPassword)
-            if (BuildConfig.DEBUG) {
+           /* if (BuildConfig.DEBUG) {
                 viewModel.loginUser("numiraaj@gmail.com", "123456")
-            } else {
+            } else {*/
                 if (email.text.toString().isEmpty() ||  password.text.toString().isEmpty() ) {
                     AppUtils.showSnackMessage(
                         "Please enter Email/Password",
@@ -66,7 +66,7 @@ class LoginActivity : AppCompatActivity() {
                     viewModel.loginUser(email.text.toString(), password.text.toString())
 
                 }
-     }
+    // }
         }
 
 
